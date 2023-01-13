5637d9a320f589a6cb712612ee6af37d4404f0ce
LucianoMessineo096 <89654809+LucianoMessineo096@users.noreply.github.com>
2023-01-13 13:55:33 +0100

    Update MachinesUtils.java

--- src/main/java/Utils/MachinesUtils.java
+++ src/main/java/Utils/MachinesUtils.java
@@ -7,6 +7,9 @@ package Utils;
 import Model.Machine;
 import Model.MachineServices;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 /**
@@ -43,6 +46,55 @@ public class MachinesUtils {
             
                 machineServices.changeStatus(machine.getId(),"disabled");
             }
+            
+        }
+    
+    }
+    
+    public void checkMachineCapacity(Machine machine) throws SQLException{
+    
+        //check if machine capacity is eq 0
+        //if eq 0 then set status=disabled 
+        
+        MachineServices machineServices = new MachineServices();
+          
+        if(machine.getActualCapacity()==0){
+
+            machineServices.changeStatus(machine.getId(),"disabled");
+        }
+            
+    }
+    
+    public void checkMachinesStatus() throws SQLException{
+    
+        /*
+            this method is called to check status for every machine
+            
+            when a user close the browser without passing to logout then the status remain pending in occupied
+        
+            how to resolve: if status=occupied for plus 10 minutes then machine is available again
+            
+        */
+        
+        MachineServices machineServices = new MachineServices();
+        ArrayList<Machine> occupiedMachines = machineServices.getMachinesByStatus("occupied");
+        
+        for(Machine machine : occupiedMachines){
+
+            String occupiedSince = machine.getOccupiedSince();
+            
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime targetTime = LocalDateTime.parse(occupiedSince, formatter);
+            
+            LocalDateTime currentTime = LocalDateTime.now();
+            long minutesBetween = ChronoUnit.MINUTES.between(currentTime, targetTime);
+
+            if (minutesBetween > 2) {
+                
+                machineServices.changeStatus(machine.getId(), "free");
+                machineServices.setToNullOccupiedSince(machine.getId());
+            }
+               
         }
     
     }
